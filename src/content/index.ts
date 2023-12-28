31bbbcf12fb0deaa06b4aad6af0350e2fb5c3ca8
denrb <rybkindenu@gmail.com>
2023-12-28 14:56:24 -0500

    init commit

---
+++ src/content/index.ts
@@ -0,0 +1,59 @@
+import { marked } from 'marked'
+import type { PersonalPageCfg } from './types'
+
+import mainText from './texts/mainText.md?raw'
+import shortDesc from './texts/shortDesc.md?raw'
+import expSber from './texts/expSber.md?raw'
+import expMorizo from './texts/expMorizo.md?raw'
+import expVMedia from './texts/expVMedia.md?raw'
+
+export * from './types'
+
+export const pageConfig: PersonalPageCfg = {
+  name: 'Denis Rybkin',
+  occupation: 'iOS Developer',
+  shortDesc: marked.parse(shortDesc).toString(),
+  resumeLink: '/Denis_Rybkin_Resume.pdf',
+  socials: [
+    {
+      href: 'mailto:denisrybkinios@gmail.com',
+      icon: ['far', 'envelope']
+    },
+    {
+      href: 'https://github.com/denis-rybkin',
+      icon: ['fab', 'github']
+    },
+    {
+      href: 'https://www.instagram.com/denisrybkin3',
+      icon: ['fab', 'instagram']
+    },
+    {
+      href: 'https://www.linkedin.com/in/denis-rybkin',
+      icon: ['fab', 'linkedin']
+    },
+  ],
+  mainText: marked.parse(mainText).toString(),
+  resume: [
+    {
+      from: 'Aug. 2021',
+      to: 'Aug. 2022',
+      occupation: 'Middle iOS Developer',
+      company: 'Sber',
+      description: marked.parse(expSber).toString()
+    },
+    {
+      from: 'Oct. 2020',
+      to: 'Aug. 2021',
+      occupation: 'Middle iOS Developer',
+      company: 'Morizo Digital',
+      description: marked.parse(expMorizo).toString()
+    },
+    {
+      from: 'Aug. 2018',
+      to: 'Oct. 2020',
+      occupation: 'Junior iOS/tvOS Developer',
+      company: 'VMedia inc.',
+      description: marked.parse(expVMedia).toString()
+    },
+  ]
+}
